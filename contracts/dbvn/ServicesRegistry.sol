017f3cf84e98290b3f289582a1a4dfe2cb51e032
DeveloppSoft <developpsoft@gmail.com>
2017-06-30 10:37:52 +0200

    Rework service registry

--- contracts/dbvn/ServicesRegistry.sol
+++ contracts/dbvn/ServicesRegistry.sol
@@ -10,13 +10,18 @@ contract ServicesRegistry {
 
 		address addr;
 		string abi;
+
+		bool enabled;
+
+		uint addedOn;
 	}
+
+	event ServiceChanged(uint id);
 
 	function ServicesRegistry() {
-		allServices.push(Service("", "", 0x0, ""));
+		allServices.push(Service("", "", 0x0, "", false, now));
 	}
 
-	function addService(string _name, string _description, address _addr, string _abi);
-
+	function addService(string _name, string _description, address _addr, string _abi) return (uint ServiceID);
 	function removeService(uint id);
 }
